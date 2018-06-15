a44cdc40641f067b3bece37f9cc1876aca51a293
Euan <tartanarmy@users.noreply.github.com>
2018-06-15 13:55:02 +0100

    Update environment

--- app.js
+++ app.js
@@ -4,20 +4,47 @@
 
 import path from 'path';
 import express from 'express';
+import logger from 'morgan';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
 
 import routes from './routes';
-
 // Express app setup
 const app = express();
-
 
 // view engine
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'pug');
 
-// use routes
-app.use('/', routes);
+// logger
+app.use(logger('combined'));
 
+// body parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+// cookie parser
+app.use(cookieParser());
+
+// serve static files from 'public'
+app.use(express.static(path.join(__dirname, './public')));
+
+// use routes
+app.use('/', routes); // <--- error 404 handler comes after this
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+// error handlers
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {},
+  });
+  next();
+});
 
 export default app;
-
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
 import express from 'express';
 
+import dotenv from 'dotenv';
+// use dotenv
+dotenv.config({
+  silent: true,
+});
+
 const router = express.Router();
 /* GET home page. */
 router.get('*', (req, res) => {
